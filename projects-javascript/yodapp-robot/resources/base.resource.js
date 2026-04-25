7eb67e2fa0d36bd2e0c30e3c4dd6b052e3068b11
Douglas Antonio <douglas.doug1235@gmail.com>
2026-04-25 16:14:28 -0300

    Implementação dos testes automatizados

--- projects-javascript/yodapp-robot/resources/base.resource.js
+++ projects-javascript/yodapp-robot/resources/base.resource.js
@@ -1,27 +1,27 @@
 class BaseResource {
     async getStarted() {
         const startText = await $(`android=new UiSelector().text("QAX")`);
-        await startText.waitForDisplayed({ timeout: 5000 });
+        await startText.waitForDisplayed({ timeout: 15000 });
         await startText.click();
     }
 
     async navigateTo(itemText) {
         const hamburger = await $('//android.widget.ImageButton[@content-desc="Open navigation drawer"]');
-        await hamburger.waitForDisplayed({ timeout: 5000 });
+        await hamburger.waitForDisplayed({ timeout: 15000 });
         await hamburger.click();
 
         const menuItem = await $(`//*[@resource-id="com.qaxperience.yodapp:id/navView"]//*[@text="${itemText}"]`);
-        await menuItem.waitForDisplayed({ timeout: 5000 });
+        await menuItem.waitForDisplayed({ timeout: 15000 });
         await menuItem.click();
     }
 
     async goToItem(item, checkpoint) {
         const itemElement = await $(`android=new UiSelector().text("${item}")`);
-        await itemElement.waitForDisplayed({ timeout: 5000 });
+        await itemElement.waitForDisplayed({ timeout: 15000 });
         await itemElement.click();
 
         const checkpointElement = await $(`android=new UiSelector().text("${checkpoint}")`);
-        await checkpointElement.waitForDisplayed({ timeout: 5000 });
+        await checkpointElement.waitForDisplayed({ timeout: 15000 });
     }
 }
 
--- projects-javascript/yodapp-robot/wdio.conf.js
+++ projects-javascript/yodapp-robot/wdio.conf.js
@@ -1,35 +1,63 @@
-const path = require('path');
-
 exports.config = {
-    runner: 'local',
-    port: 4723,
+
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY,
+    region: 'us',
+
     specs: [
         './tests/**/*.spec.js'
     ],
+    exclude: [],
+
+
+
     maxInstances: 1,
     capabilities: [{
         platformName: 'Android',
-        'appium:deviceName': 'Android Emulator',
         'appium:automationName': 'UiAutomator2',
-        'appium:app': path.join(process.cwd(), '../../projects/yodapp-robot/app/yodapp-beta.apk'),
-        'appium:udid': 'emulator-5554',
-        'appium:autoGrantPermissions': true
+        'appium:app': 'storage:filename=yodapp-beta.apk',
+
+        'appium:deviceName': '.*',
+        'appium:platformVersion': '.*',
+
+        'sauce:options': {
+            build: 'appium-build-qax-yodapp',
+            name: 'Teste Cabuloso do Yodapp',
+            deviceOrientation: 'portrait'
+        }
     }],
+
     logLevel: 'info',
-    waitforTimeout: 5000,
+    bail: 0,
+    baseUrl: 'http://localhost',
+    waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
-    services: [],
+
+    services: ['sauce'],
+
     framework: 'mocha',
     reporters: ['spec'],
+
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
     },
-    afterTest: async function (test, context, { error, result, duration, passed, retries }) {
-        await driver.terminateApp('com.qaxperience.yodapp');
+
+
+    before: async function (capabilities, specs) {
     },
+
     beforeTest: async function (test, context) {
         await driver.activateApp('com.qaxperience.yodapp');
+    },
+
+    afterTest: async function (test, context, { error, result, duration, passed, retries }) {
+        if (!passed) {
+            await browser.execute('sauce:job-result=failed');
+        } else {
+            await browser.execute('sauce:job-result=passed');
+        }
+        await driver.terminateApp('com.qaxperience.yodapp');
     }
 }
